1565cae3c8db3a36fadec6b409abe4b7adf9f57f
shubhansu31 <jainshubhansu31@gmail.com>
2021-03-08 23:41:34 +0530

    api call added

--- app/src/main/java/com/e/chess_fo/MainActivity.kt
+++ app/src/main/java/com/e/chess_fo/MainActivity.kt
@@ -5,6 +5,13 @@ import android.os.Bundle
 import android.view.View
 import android.widget.EditText
 import android.widget.Toast
+import com.android.volley.Request
+import com.android.volley.Request.*
+import com.android.volley.Request.Method.*
+import com.android.volley.Response
+import com.android.volley.toolbox.JsonObjectRequest
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
 
 class MainActivity : AppCompatActivity() {
     private lateinit var textName : EditText;
@@ -18,6 +25,21 @@ class MainActivity : AppCompatActivity() {
 
     fun getInfo(view: View) {
         val userName : String = textName.text.toString();
-        Toast.makeText(this,userName,Toast.LENGTH_LONG).show();
+        val countryName = call(userName);
+        Toast.makeText(this,"User: $userName  Country: $countryName",Toast.LENGTH_LONG).show();
+    }
+
+    fun call(userName: String):String{
+        lateinit var name : String;
+        val queue = Volley.newRequestQueue(this);
+        val url = "https://api.chess.com/pub/player/$userName";
+        val jsonObjectRequest = JsonObjectRequest(Request.Method.GET, url,null,
+            { response ->
+                name = response.getString("country");
+            },
+            Response.ErrorListener {name = "That didn't work!"; }
+        )
+        queue.add(jsonObjectRequest);
+        return name;
     }
 }
