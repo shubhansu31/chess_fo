548625bcd8403038574d8eb2cff97c02c1d23a52
shubhansu31 <jainshubhansu31@gmail.com>
2021-03-08 23:05:57 +0530

    no api added

--- app/src/main/java/com/e/chess_fo/MainActivity.kt
+++ app/src/main/java/com/e/chess_fo/MainActivity.kt
@@ -2,10 +2,22 @@ package com.e.chess_fo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.EditText
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var textName : EditText;
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        textName = findViewById(R.id.textPersonName);
+
+    }
+
+    fun getInfo(view: View) {
+        val userName : String = textName.text.toString();
+        Toast.makeText(this,userName,Toast.LENGTH_LONG).show();
     }
 }
